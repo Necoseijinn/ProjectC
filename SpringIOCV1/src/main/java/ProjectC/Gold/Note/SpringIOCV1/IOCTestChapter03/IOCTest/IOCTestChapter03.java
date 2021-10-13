af49498a58ea8708736f929e8bafb3109c5f60d6
necoseijinn <necoseijinn@gmail.com>
2021-10-13 18:29:33 +0900

    2021/10/13 Modify SpringIOCV1

--- SpringIOCV1/src/main/java/ProjectC/Gold/Note/SpringIOCV1/IOCTestChapter03/IOCTest/IOCTestChapter03.java
+++ SpringIOCV1/src/main/java/ProjectC/Gold/Note/SpringIOCV1/IOCTestChapter03/IOCTest/IOCTestChapter03.java
@@ -1,5 +1,9 @@
 package ProjectC.Gold.Note.SpringIOCV1.IOCTestChapter03.IOCTest;
 
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /***
  *
  * test01
@@ -7,11 +11,28 @@ package ProjectC.Gold.Note.SpringIOCV1.IOCTestChapter03.IOCTest;
  *      创建带有生命周期方法的bean
  *          生命周期：bean的创建到销毁
  * 		    ioc容器中注册的bean：
- * 		    	1，单例bean，容器启动的时候就会创建好，容器关闭也会销毁创建的bean
- * 		    	2，多实例bean，获取的时候才创建
- * 		    我们可以为bean自定义一些生命周期方法；Spring在创建或者销毁的时候就会调用指定的方法
- * 		    自定义初始化方法和销毁方法； The method must have no arguments,but may throw any exception
+ * 		    1）单例bean
+ * 		        （容器启动的途中）构造器被调用 ---〉 bean定义的初始化方法 ---〉 （容器关闭的途中）bean定义的销毁方法
+ * 		    2）多例bean
+ * 		        （获取bean的时候）构造器被调用 ---〉 bean定义的初始化方法 ---〉 （GC并且被判定为垃圾的时候）bean定义的销毁方法
+ * 		        注意容器关闭并不会调用多例bean定义的销毁方法
+ *
+ * 		    我们可以自定义bean的初始化方法和销毁方法来做一些bean的初始化操作和销毁操作
+ * 		    初始化方法和销毁方法必须满足下面的要求
+ * 		        The method must have no arguments,but may throw any exception
+ *          简单来说就是初始化和销毁方法不能有参数
  *
  */
 public class IOCTestChapter03 {
+
+    private ApplicationContext applicationContext;
+
+    @Test
+    public void test01(){
+
+        applicationContext = new ClassPathXmlApplicationContext("iocTestChapter03.xml");
+
+
+    }
+
 }
---
+++ SpringIOCV1/src/main/java/ProjectC/Gold/Note/SpringIOCV1/IOCTestChapter03/pojo/Worker.java
@@ -0,0 +1,37 @@
+package ProjectC.Gold.Note.SpringIOCV1.IOCTestChapter03.pojo;
+
+import java.util.Date;
+
+public class Worker {
+
+    private String name;
+    private int age;
+
+    public Worker() {
+        System.out.println(new Date().toString()+" [Worker]的构造函数被调用了");
+    }
+
+    public void workerInitMethod(){
+        System.out.println(new Date().toString()+" [Worker]的初始化函数被调用了");
+    }
+
+    public void workerDestroyMethod(){
+        System.out.println(new Date().toString()+" [Worker]的销毁函数被调用了");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
